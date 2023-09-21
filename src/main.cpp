6dd12257533ce7f4f18f5065e2c4063c8c9a483f
Spencer Frisby <sgfrisby@duck.com>
2023-09-21 17:12:36 -0600

    Instance created.

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,17 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <string>
 #include <iostream>
 #include <stdexcept>
+#include <vector>
 #include <cstdlib>
 
-class HelloTriangleApplication {
+class HelloTriangleApplication
+{
 public:
-    void run() {
+    void run()
+    {
         initWindow();
         initVulkan();
         mainLoop();
@@ -16,11 +20,13 @@ public:
     }
 
 private:
-    GLFWwindow* window;
+    VkInstance instance;
+    GLFWwindow *window;
     const uint32_t WIDTH = 800;
     const uint32_t HEIGHT = 600;
 
-    void initWindow() {
+    void initWindow()
+    {
         glfwInit();
 
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
@@ -29,29 +35,102 @@ private:
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
 
-    void initVulkan() {
-
+    void initVulkan()
+    {
+        createInstance();
     }
 
-    void mainLoop() {
-        while (!glfwWindowShouldClose(window)) {
+    void createInstance()
+    {
+
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char **glfwExtensions;
+
+        if (!glfwVulkanSupported())
+        {
+            std::cout << "GLFW DOES NOT SUPPORT VULKAN ON THE SYSTEM!" << '\n';
+        }
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        // *&extension.extensionName is char[256]
+        // extensions (vulkan) contains 13. The name may be obtained via:
+        //   std::str vulkanExtensionName(*&extension.extensionName);
+        // glfwExtensionCount is 2. The name may be obtained via:
+        //   std::str glfwExtensionName(*&glfwExtensions[i]);
+
+        std::cout << "available extensions:\n";
+        for (const auto &extension : extensions)
+        {
+            std::string vulkanExtensionName(*&extension.extensionName);
+            std::cout << '\t' << extension.extensionName << '\n';
+            for (int i = 0; i < glfwExtensionCount; i++)
+            {
+                std::string glfwExtensionName(*&glfwExtensions[i]);
+                if (glfwExtensionName == vulkanExtensionName)
+                {
+                    std::cout << '\t' << '\t' << "* supported by GLFW *" << '\n';
+                }
+            }
+        }
+        int foo = 0;
+
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+        {
+            throw std::runtime_error("failed to create instance!");
+        }
+    }
+
+    void mainLoop()
+    {
+        while (!glfwWindowShouldClose(window))
+        {
             glfwPollEvents();
         }
     }
 
-    void cleanup() {
+    void cleanup()
+    {
+        vkDestroyInstance(instance, nullptr);
+
         glfwDestroyWindow(window);
 
         glfwTerminate();
     }
 };
 
-int main() {
+int main()
+{
     HelloTriangleApplication app;
 
-    try {
+    try
+    {
         app.run();
-    } catch (const std::exception& e) {
+    }
+    catch (const std::exception &e)
+    {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
